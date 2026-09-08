f5d5b782e5792832bbb0da9b56f85433d0ae30fa
Tyler Thibault <tyler.thibault@protonmail.com>
2026-09-08 11:59:53 -0700

    Sprint 2.3: export work item writer

--- src/services/work-items/index.ts
+++ src/services/work-items/index.ts
@@ -2,3 +2,5 @@ export * from "./WorkItemParseResult";
 export * from "./WorkItemParser";
 export * from "./WorkItemScanner";
 export * from "./WorkItemValueNormalizer";
+export * from "./WorkItemWritePatch";
+export * from "./WorkItemWriter";
