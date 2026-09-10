833e3bb1657ccbee8c3f0781af03a908abdd37ac
Tyler Thibault <tyler.thibault@protonmail.com>
2026-09-10 11:33:35 -0700

    Allow linked Base on work items

--- src/models/work-item/WorkItemSchema.ts
+++ src/models/work-item/WorkItemSchema.ts
@@ -28,7 +28,8 @@ const COMMON_OPTIONAL_PROPERTIES = [
   "duration",
   "completed",
   "parent",
-  "dependsOn"
+  "dependsOn",
+  "linkedBase"
 ] as const satisfies readonly WorkItemPropertyKey[];
 
 export const WORK_ITEM_TYPE_SCHEMAS: Readonly<Record<WorkItemType, WorkItemTypeSchema>> = {
